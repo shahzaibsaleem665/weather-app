c7fdb60e82441d906d4fb3b98c1a5448e6ebbd28
shahzaib saleem <shahzaibsaleem665@gmail.com>
2023-11-26 02:49:54 +1100

    completed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,49 +1,137 @@
-import React, {useState} from 'react'
+import  {React, useEffect, useState} from 'react'
 import './Home.css';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { Air, SearchOutlined, Water, Thermostat} from '@mui/icons-material';
 import axios from 'axios';
 
 
 
 function Home() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState({});
+  const [suggestions, setSuggestions] = useState([]);
+  const geoApi= `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=95f678220a4ce901983d937fd84be6ca`
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&exclude=hourly,daily&appid=95f678220a4ce901983d937fd84be6ca`;
+  const suggestionApi = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=95f678220a4ce901983d937fd84be6ca`;
 
-//   const url = `http://api.openweathermap.org/geo/1.0/direct?q=Lahore&limit=5&appid=95f678220a4ce901983d937fd84be6ca
-// `
+
+  const getWeatherData = () => {
+    axios.get(url)
+      .then(response => {
+        setWeatherData(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+      });
+  };
+  
+  const searchLocation = () => {  
+    axios.get(geoApi).then((response) => {
+      const firstResult = response.data[0];
+      if (firstResult) {
+        setData(firstResult);
+        setLocation('');
+  
+        getWeatherData(); // Call getWeatherData here as that data api doens't accpet location directly but longitude and latitude.
+      }
+      console.log(response.data);
+    }).catch(error => alert(error.message));
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      searchLocation();
+      setSuggestions([]);
+      
+    } 
+  }
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    setLocation(inputValue);
+
+    if (inputValue.trim() !== '') {
+      axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=5&appid=95f678220a4ce901983d937fd84be6ca`)
+      .then(response => {
+        setSuggestions(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching suggestions:', error);
+      });
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSuggestionClick = (suggest) => {
+    setLocation(suggest.name);
+    setSuggestions([]); // Hide suggestions after selection
+    searchLocation();
+  };
+
+
+// function to change temperature valus to celcius as the api renders the value in Kelvin
+  const inCelcius = (kelvin) => {
+    const celcius =  kelvin - 273.15;
+    return Math.round(celcius); // function to round the decimal number
+  };
+
+  //function to change wind speed to KMH an round off the decimals as the API gives wind spee in m/s.
+
+  const convertWindSpeed = (metersPerSecond, decimalPlaces = 2) => {
+    const kmPerHour = metersPerSecond * 3.6;
+    return Math.round(kmPerHour.toFixed(decimalPlaces));
+  };
+  
+  useEffect(() => {
+    if (data.lat && data.lon) {
+      getWeatherData();
+    }
+  },[data]);
+ 
   return (
     <div className='home'>
-        <div className="home__weather">
+        <div className='home__weather'>
             <div className="weather__top">
-            <LocationOnIcon />
-            <input type='text'  placeholder='enter city name'></input>
-            <SearchOutlined />
+            <input type='text' value={location} onChange={handleInputChange}  placeholder='enter city name' onKeyDown={handleKeyPress} required />
+            <SearchOutlined onClick={searchLocation}/>
             </div>
+            {suggestions.length > 0 && (
+        <ul className="suggestions-list">
+           {suggestions.map((suggest) => (
+            <li key={suggest.name} onClick={() => handleSuggestionClick(suggest)}>
+              {suggest.name}, {suggest.country}
+            </li>
+          ))}
+        </ul>
+      )}
             <div className="weather__middle">
               <div className="weather__middleLocation">
-                <p>Sydney</p>
+               <h3>{data.name}</h3>
               </div>
               <div className="weather__middleTemp">
-                <h1>60 C</h1>
+              <h1>{weatherData.main && inCelcius(weatherData.main.temp)}°C</h1>
               </div>
               <div className="weather__middleDescription">
-                <p>Cloudy</p>
+                <h3>{weatherData.weather && weatherData.weather[0].description}</h3>
               </div>
             </div>
             <div className="weather__bottom">
               <div className="weather__bottomLeft">
               <Water />
                 <span>Humidity</span>
-                <p>16%</p>
+                <p>{weatherData.main && weatherData.main.humidity}%</p>
               </div>
               <div className="weather__bottomMiddle">
                 <Thermostat />
-                <span>Feels</span>
-                <p>65 C</p>
+                <span>Feels like</span>
+                <p>{weatherData.main && inCelcius(weatherData.main.feels_like)}°C</p>
               </div>
               <div className="weather__bottomRight">
               <Air />
-                <span>Wind speed</span>
-                <p>20 MPH</p>
+                <span>Wind</span>
+                <p>{weatherData.wind && convertWindSpeed(weatherData.wind.speed)} KMH</p>
               </div>
             </div>
         </div>
@@ -52,3 +140,4 @@ function Home() {
 }
 
 export default Home
+
