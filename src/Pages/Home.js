606c43de76e71e5336cf7e3febd5198b304de375
shahzaib saleem <shahzaibsaleem665@gmail.com>
2023-11-25 19:56:25 +1100

    working on styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import './Home.css'
+import React, {useState} from 'react'
+import './Home.css';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import { Air, Search, SearchOutlined, Water } from '@mui/icons-material';
-import { Input } from '@mui/material';
+import { Air, SearchOutlined, Water, Thermostat} from '@mui/icons-material';
+import axios from 'axios';
+
 
 
 function Home() {
+
+//   const url = `http://api.openweathermap.org/geo/1.0/direct?q=Lahore&limit=5&appid=95f678220a4ce901983d937fd84be6ca
+// `
   return (
     <div className='home'>
         <div className="home__weather">
@@ -15,16 +19,31 @@ function Home() {
             <SearchOutlined />
             </div>
             <div className="weather__middle">
-              <h2>Error!!</h2>
+              <div className="weather__middleLocation">
+                <p>Sydney</p>
+              </div>
+              <div className="weather__middleTemp">
+                <h1>60 C</h1>
+              </div>
+              <div className="weather__middleDescription">
+                <p>Cloudy</p>
+              </div>
             </div>
             <div className="weather__bottom">
               <div className="weather__bottomLeft">
               <Water />
                 <span>Humidity</span>
+                <p>16%</p>
+              </div>
+              <div className="weather__bottomMiddle">
+                <Thermostat />
+                <span>Feels</span>
+                <p>65 C</p>
               </div>
               <div className="weather__bottomRight">
               <Air />
                 <span>Wind speed</span>
+                <p>20 MPH</p>
               </div>
             </div>
         </div>
